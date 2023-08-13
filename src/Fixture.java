e11df1e7daac064beab24117aff5fb55204d0fc6
msametselvi@gmail.com <msametselvi@gmail.com>
2023-08-14 02:50:19 +0300

    second commit

--- src/Fixture.java
+++ src/Fixture.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Fixture {
+import java.util.*;
+
+public class Fixture {
+    private List<String> teams;
+
+    public Fixture(List<String> teams){
+        this.teams= teams;
+    }
+
+
+    public List<String> generateFixtures(){
+        List<String> fixtures = new ArrayList<>();
+
+
+
+
+        List<String> shuffledTeams = new ArrayList<>(teams);
+
+        for(int dist = 1; dist < shuffledTeams.size(); dist++){
+
+            for(int i = 0; i + dist < shuffledTeams.size(); i++){
+                System.out.println(shuffledTeams.get(i) + " - " + shuffledTeams.get(i+dist));
+            }
+        }
+        System.out.println("---------------Deplasmanları--------");
+        for(int a = 1; a < shuffledTeams.size(); a++){
+            for(int i = 0; i + a < shuffledTeams.size(); i++){
+
+                System.out.println(shuffledTeams.get(i+a) + " - " + shuffledTeams.get(i) );
+
+            }
+        }
+
+
+        return shuffledTeams;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,33 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        OrderByTotalPageComparator orderByTotalPageComparator= new OrderByTotalPageComparator();
-        OrderByNameComparator orderByNameComparator = new OrderByNameComparator();
-        TreeSet<Book> bookTreeSet = new TreeSet<>(orderByTotalPageComparator);
-       // TreeSet<Book> bookTreeSet = new TreeSet<>(orderByNameComparator);
+
+        ArrayList <String> teams = new ArrayList<>();
+
+        teams.add("GS");
+        teams.add("Fenerbahçe");
+       teams.add("TS");
+       teams.add("Bursaspor");
+       teams.add("BJK");
+        teams.add("Başakşehir");
 
 
-        bookTreeSet.add(new Book("Tutunamayanlar", 650, "Oğuz Atay", "26.05.1997"));
-        bookTreeSet.add(new Book("Kiralık Konak", 232, "Oğuz Atay", "1922"));
-        bookTreeSet.add(new Book("Yaprak Dökümü", 160, "Reşat Nuri Güntekin", "1930"));
-        bookTreeSet.add(new Book("Kürk Mantolu Madonna", 159, "Reşat Nuri Güntekin", "1943 "));
-        bookTreeSet.add(new Book("Amok Koşucusu", 60 ,"Stefan Zweig", "1922"));
-
-        for (Book a : bookTreeSet
-        ) {
-            System.out.println(a.getName());
+        if (teams.size() % 2 != 0) {
+            teams.add("Bay");
         }
+        Fixture fixture = new Fixture(teams);
+
+
+        System.out.println(fixture.generateFixtures());
+
+
+
+
+
+
 
 
     }
+
 
 }
